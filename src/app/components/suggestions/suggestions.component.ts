d61303b1b378a4f18435d436a1eb6d201a36db99
borgella <jmborgella@hotmail.com>
2016-12-04 02:53:29 -0500

    added suggestions component file

---
+++ src/app/components/suggestions/suggestions.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { AppProfileService } from '../../service/app.auth.service';
+import { HttpService } from '../../service/app.http.service';
+import { Auth0HttpService } from '../../service/app.authHttp.service';
+
+@Component({
+  selector: 'app-suggestions',
+  templateUrl: './suggestions.component.html',
+  styleUrls: ['./suggestions.component.css']
+})
+export class SuggestionsComponent {
+  constructor(private _httpservice: HttpService, private auth: AppProfileService, protected _auth0: Auth0HttpService) { }
+
+  public subscribeToUser(user) {
+    this._httpservice.putAbonnement(this.auth.user._id, user._id).subscribe((res) => {
+
+      this._httpservice.getAbonnements(this.auth.user._id).subscribe((response) => {
+        this.auth.user.subscribers = response.body;
+      }, (error) => console.log('we will take care of this error later ' + error));
+
+      this._httpservice.getSuggestions(this.auth.user._id).subscribe((resp) => {
+        this.auth.user.suggestions = resp.body;
+      });
+
+    }, (error) => console.log(' handle error event' + error ));
+  }
+}
