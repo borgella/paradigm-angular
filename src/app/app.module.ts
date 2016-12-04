59002aeaa640f235a460fb4d10e4514806de4f47
borgella <jmborgella@hotmail.com>
2016-12-04 02:44:07 -0500

    provided services in appModule class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { provideAuth } from 'angular2-jwt';
+import { HttpService } from './service/app.http.service';
+import { AppProfileService } from './service/app.auth.service';
+import { Auth0HttpService } from './service/app.authHttp.service';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,7 +19,15 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [HttpService, AppProfileService, Auth0HttpService, provideAuth({
+      headerName: 'Authorization',
+      headerPrefix: 'Bearer',
+      tokenName: 'id_token',
+      tokenGetter: ( () => localStorage.getItem('id_token') ),
+      globalHeaders: [{'Content-Type': 'application/json'}],
+      noJwtError: true,
+      noTokenScheme: true
+    })],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
