80583b511109ca45f5fd5e59bc7d015aa803bfe0
borgella <jmborgella@hotmail.com>
2016-12-05 17:20:35 -0500

    fetch for suggestion in profil component

--- src/app/components/profil/profil.component.ts
+++ src/app/components/profil/profil.component.ts
@@ -12,19 +12,24 @@ export class ProfilComponent {
 
     public constructor(private auth_profil: AppProfileService, private _auth0: Auth0HttpService, private _httpservice: HttpService) {
         this.auth_profil.setProfile(false);
-        this._httpservice.getAbonnements(this.auth_profil.getUserId())
-            .subscribe( (data) => {
+        this._httpservice.getAbonnements(this.auth_profil.user._id)
+            .subscribe((data) => {
                 this.auth_profil.user.setSubscribers(data.body);
             }, (error) => console.log('we will take care of the get abonnements error'));
     }
 
     public unSubscribeToUser(abonne) {
-        this._httpservice.deleteAbonnement(this.auth_profil.getUserId(), abonne._id)
+        console.log(this.auth_profil.user._id, abonne._id);
+        this._httpservice.deleteAbonnement(this.auth_profil.user._id, abonne._id)
             .subscribe((data) => {
                 this._httpservice.getAbonnements(this.auth_profil.getUserId())
-                    .subscribe( (datas) => {
-                        this.auth_profil.user.setAbonnements(datas.body);
+                    .subscribe((datas) => {
+                        this.auth_profil.user.subscribers = datas.body;
                     }, (error) => console.log('we will take care of delete get abonnements error'));
+
+                this._httpservice.getSuggestions(this.auth_profil.user._id).subscribe((resp) => {
+                    this.auth_profil.user.suggestions = resp.body;
+                });
 
             }, (error) => console.log('we will take care of delete abonnements error'));
     }
