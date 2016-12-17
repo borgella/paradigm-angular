94528657395fec7183aa43fa9885fd275e2cb24f
borgella <jmborgella@hotmail.com>
2016-12-17 15:40:43 -0500

    switched app.components for accueil component

--- src/app/components/accueil/accueil.component.ts
+++ src/app/components/accueil/accueil.component.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { OnInit } from '@angular/core';
 import { AppProfileService } from '../../service/app.auth.service';
-import { SuggestionsComponent } from '../suggestions/suggestions.component';
+import { Auth0HttpService } from '../../service/app.authHttp.service';
+import { HttpService } from '../../service/app.http.service';
 
 @Component({
     selector: 'app-accueil',
     templateUrl: './accueil.component.html',
-    providers: [SuggestionsComponent]
+    styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent {
-    constructor(private auth_profil: AppProfileService) {
-        this.auth_profil.setProfile(true);
-    }
+export class AccueilComponent implements OnInit {
+      userform: FormGroup;
+  private value: string;
+  profilValue: boolean = false;
+
+
+  constructor(private _formBuilder: FormBuilder, private app_auth: AppProfileService, 
+          private auth0: Auth0HttpService, private _httpservice: HttpService) { }
+
+  public ngOnInit() {
+    this.userform = this._formBuilder.group({
+      text: ['', Validators.required]
+    });
+  }
+
+  public postAtweet() {
+    this._httpservice.postTweet(this.app_auth.user._id, this.createTweet(this.userform.value.text))
+      .subscribe((body) => {
+
+        this._httpservice.getFil(this.app_auth.user._id)
+          .subscribe((datas) => {
+            this.app_auth.user.tweets = datas.body.userTweets.reverse();
+            this.app_auth.user.retweets = datas.body.userRetweets.reverse();
+          }, (error) => console.log(' error get fil ' + error));
+      }, (error) => console.log('error post tweet ' + error));
+    this.resetForm();
+  }
+
+  public setValue(value) {
+    console.log(value);
+    this.value = value;
+  }
+
+  public createTweet(text) {
+    return {
+      date: Date.now,
+      text: text
+    };
+  }
+
+  public resetForm() {
+    this.userform.reset();
+  }
 }
